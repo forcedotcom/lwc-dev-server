c4dc3c2c4dea446fefdf6fb08aa4a7209dc5dab2
Min Idzelis <midzelis@salesforce.com>
2019-04-12 10:10:19 -0600

    Add test projects: todomvc, ebikes

---
+++ test-projects/lwc-todomvc-e8217b7/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    preset: '@lwc/jest-preset',
+    moduleNameMapper: {
+        '^(todo)/(.+)$': '<rootDir>/src/$1/$2/$2'
+    },
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test/specs/']
+};
