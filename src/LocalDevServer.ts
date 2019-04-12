534aa2d887c22d5eb60649eb343e71abf8982c33
Min Idzelis <midzelis@salesforce.com>
2019-04-12 15:43:31 -0600

    Server launches and auto compiles

--- src/LocalDevServer.ts
+++ src/LocalDevServer.ts
@@ -1,29 +1,3 @@
-/*
-{
-  "name": "my-ebikes-app",
-  "version": "0.0.1",
-  "scripts": {
-    "clean": "rm -rf dist",
-    "build:talon": "talon generate .",
-    "build:public": "node scripts/copy-resources.js",
-    "build": "npm run build:public && npm run build:talon",
-    "serve": "MODE=\"${MODE:-dev}\" talon run .",
-    "start": "npm run build:public --silent -- --watch & npm run serve --silent -- "
-  },
-  "devDependencies": {
-    "@lwc/engine": "0.37.0",
-    "@salesforce-ux/design-system": "^2.6.1",
-    "@talon/compiler": "0.0.13",
-    "@talon/force-modules": "^0.0.5",
-    "colors": "^1.3.2",
-    "cpx": "^1.5.0",
-    "lwc-components-lightning": "1.1.6-alpha"
-  },
-  "engines": {
-    "node": ">=10.0"
-  }
-}
-*/
 import path from 'path';
 import colors from 'colors';
 //import performance from 'perf_hooks';
@@ -35,7 +9,7 @@ import {
 import metadataService from '@talon/compiler/src/metadata/metadata-service';
 import resourceService from '@talon/compiler/src/resources/resource-service';
 import validate from '@talon/compiler/src/metadata/metadata-validation';
-import { isMainThread } from 'worker_threads';
+import { run } from '@talon/compiler/src/server/server';
 
 export default class LocalDevServer {
     public install() {}
@@ -48,32 +22,44 @@ export default class LocalDevServer {
         // Find where all the source code is.
         // This should have /lwc on the end, but I think the talon compiler expects the directory name to be the namespace passed
         // to the descriptor.
-        const lwc_directory = directory;
 
-        // Pass that to the Talon compiler.
-        await this.run(`component://${entryPoint}@en`, lwc_directory);
-
-        // Start the talon site.
-    }
-
-    private async run(descriptor: string, templateDir: string) {
-        // const t0 = performance.now();
-        let exitCode = 0;
-        debugger;
         const config = {
-            templateDir,
-            talonConfigJson: {},
-            srcDir: `${templateDir}/src`,
-            viewsDir: null,
+            templateDir: directory,
+            talonConfigJson: path.join(
+                __dirname,
+                'config',
+                'talon.config.json'
+            ),
+            srcDir: `${directory}/src`,
+            viewsDir: path.join(__dirname, 'config', 'views'),
             indexHtml: path.join(__dirname, 'config', 'index.html'),
             routesJson: path.join(__dirname, 'config', 'routes.json'),
             labelsJson: path.join(__dirname, 'config', 'labels.json'),
             themeJson: path.join(__dirname, 'config', 'theme.json'),
-            outputDir: `${templateDir}/dist`,
+            outputDir: `${directory}/dist`,
             locale: `en_US`,
             basePath: ``,
             isPreview: true
         };
+        const descriptor = `component://${entryPoint}@en`;
+
+        // Pass that to the Talon compiler.
+
+        // Uhhh.... this is apparently totally optional, server will compile if necessary automatically
+
+        //await this.compile(config, descriptor);
+
+        const options = {
+            ...config,
+
+            indexHtml: path.join(__dirname, 'config', 'index.html')
+        };
+        // Start the talon site.
+        await run(options, 3333, '' /*apiEndpoint*/, true /*recordApiCalls*/);
+    }
+
+    private async compile(config: any, descriptor: string) {
+        // const t0 = performance.now();
 
         // valdiates metdata, we definately don't need this
         // await validate();
