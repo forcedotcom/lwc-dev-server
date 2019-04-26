a7b3dc060588e5df0b752bee22c085e7ba6f9479
Min Idzelis <midzelis@salesforce.com>
2019-04-26 11:11:33 -0600

    un-monorepo

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-    transformIgnorePatterns: [
-        '<rootDir>.*(node_modules)(?!.*local-dev-tools.*).*$'
-    ],
-    projects: ['<rootDir>/packages/local-dev-server/jest.config.js']
+    roots: ['<rootDir>/src'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testPathIgnorePatterns: ["/node_modules/", "/modules/"],
+    collectCoverage: true,
+    coverageReporters: ['json', 'html', 'text'],
+    collectCoverageFrom: ['src/**/*.ts', 'src/**/*.js'],
+    coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/test-projects/',
+        '/modules/'
+    ]
 };
