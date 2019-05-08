5698a38725c5d215ea165ff2552f31767d9ed54b
Nathan McWilliams <nmcwilliams@salesforce.com>
2019-05-08 16:09:59 -0400

    rework assets
    rework slds and other web assets so that they are not dependent on a relative node_modules location
    
    @W-6067203@

--- script/copy-assets.ts
+++ script/copy-assets.ts
@@ -1,4 +1,25 @@
-import * as shell from 'shelljs';
+import path from 'path';
+import { cp, mkdir } from 'shelljs';
 
-// Copy all the view templates
-shell.cp('-R', 'src/config/', 'dist/config/');
+const src = path.join(__dirname, '../src');
+const dest = path.join(__dirname, '../dist');
+mkdir('-p', dest);
+
+// copy non-compiled files from src to dist
+cp('-R', `${src}/assets`, dest);
+cp('-R', `${src}/config`, dest);
+
+// copy SLDS assets
+const sldsPath = path.join(
+    __dirname,
+    '../node_modules/@salesforce-ux/design-system'
+);
+
+mkdir('-p', `${dest}/slds`);
+cp('-R', `${sldsPath}/assets/styles`, `${dest}/slds`);
+
+mkdir('-p', `${dest}/slds/fonts`);
+cp('-R', `${sldsPath}/assets/fonts/webfonts`, `${dest}/slds/fonts`);
+
+mkdir('-p', `${dest}/slds/icons`);
+cp('-R', `${sldsPath}/assets/icons/*-sprite`, `${dest}/slds/icons`);
--- src/LocalDevServer.ts
+++ src/LocalDevServer.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import cpx from 'cpx';
-import mkdirp from 'mkdirp';
+import { cp, mkdir, rm } from 'shelljs';
 import {
     startContext,
     endContext
@@ -11,7 +10,6 @@ import resourceService from '@talon/compiler/src/resources/resource-service';
 import validate from '@talon/compiler/src/metadata/metadata-validation';
 import { createServer, startServer } from './talonServerCopy';
 import Project from './common/Project';
-import rimraf from 'rimraf';
 import ComponentIndex from './common/ComponentIndex';
 
 const talonConfig = {
@@ -140,7 +138,7 @@ export default class LocalDevServer {
         // fixme: clear outputDir for now because of a caching issue
         // with talon (maybe we need to force a recompile of the views?)
         if (fs.existsSync(config.outputDir)) {
-            rimraf.sync(config.outputDir);
+            rm('-rf', config.outputDir);
             console.log('cleared output dir');
         }
 
@@ -168,39 +166,21 @@ export default class LocalDevServer {
             await startServer(server, '', project.getConfiguration().port);
         } catch (e) {
             console.error(e);
-            process.exit(0);
+            process.exit(1);
         }
     }
 
     public async copyAssets(dest: string) {
-        const assetsDir = path.join(dest, 'public', 'assets');
+        const distPath = path.join(__dirname, '.');
+        const assetsPath = path.join(dest, 'public', 'assets');
 
-        mkdirp.sync(assetsDir);
-
-        // Copy Slds
-        // Whats the right thing to do here though?
-        this.copy(
-            'node_modules/@salesforce-ux/design-system/assets/**/symbols.svg',
-            assetsDir
-        );
-        this.copy(
-            'node_modules/@salesforce-ux/design-system/assets/**/*.{woff2,css}',
-            assetsDir
-        );
-
-        // Favicon
-        // Prevents an exception in raptor code when requesting a file that doesn't exist.
-        this.copy('src/assets/favicon.ico', assetsDir);
-    }
-
-    private copy(src: string, dest: string) {
-        cpx.copy(path.join(__dirname, '..', src), dest, (e: Error) => {
-            if (e === undefined || e === null) {
-                console.log(`Done copying ${src} to ${dest}`);
-            } else {
-                console.error(`Error copying ${src} to ${dest}: ${e}`);
-            }
-        });
+        try {
+            mkdir('-p', assetsPath);
+            cp('-R', `${distPath}/slds`, assetsPath);
+            cp('-R', `${distPath}/assets/*`, assetsPath);
+        } catch (e) {
+            console.error(`warning - unable to copy assets: ${e}`);
+        }
     }
 
     private pathRewrite(localPath: string) {
