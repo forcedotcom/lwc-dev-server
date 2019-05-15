a93e82d660b6eca167e92d6e9501c8b805adf225
Kris <kgray@salesforce.com>
2019-05-14 18:45:52 -0700

    Tests for Project configuration

--- src/common/__tests__/Project.test.ts
+++ src/common/__tests__/Project.test.ts
@@ -1,5 +1,6 @@
 import Project from '../Project';
 import mock from 'mock-fs';
+import SfdxConfiguration from '../../user/SfdxConfiguration';
 
 describe('project', () => {
     // Stop mocking 'fs' after each test
@@ -80,10 +81,6 @@ describe('project', () => {
             expect(project.getModuleSourceDirectory()).toBe('my-project/src');
         });
 
-        test.todo(
-            'when project is an sfdx project, then we can infer the modules directory from the packagedModules property'
-        );
-
         test('returns null when the project is invalid', () => {
             mock({
                 'my-project': {
@@ -93,6 +90,104 @@ describe('project', () => {
             });
             const project = new Project('.');
             expect(project.getModuleSourceDirectory()).toBe(null);
+        });
+    });
+
+    describe('project creation with SfdxConfiguration', () => {
+        test('when creating from sfdxconfiguration, then resolve to the sfdx-project directory', () => {
+            mock({
+                'my-project': {
+                    'sfdx-project.json': '{}',
+                    'localdevserver.config.json': '{}'
+                }
+            });
+            const sfdxConfiguration = new SfdxConfiguration('my-project/');
+            const project = new Project(sfdxConfiguration);
+            expect(project.getDirectory()).toBe('my-project/');
+        });
+
+        test('when creating from sfdxconfiguration, then configuration passed in is stored', () => {
+            mock({
+                'my-project': {
+                    'sfdx-project.json': '{}',
+                    'localdevserver.config.json': '{}'
+                }
+            });
+            const sfdxConfiguration = new SfdxConfiguration('my-project/');
+            const project = new Project(sfdxConfiguration);
+            expect(project.getSfdxConfiguration()).toBe(sfdxConfiguration);
+        });
+
+        test('when creating from sfdxconfiguration, then use the packageDirectories to resolve the module directory', () => {
+            mock({
+                'my-project': {
+                    'sfdx-project.json': JSON.stringify({
+                        packageDirectories: [
+                            {
+                                path: 'force-app',
+                                default: true
+                            }
+                        ]
+                    }),
+                    'localdevserver.config.json': '{}'
+                }
+            });
+            const sfdxConfiguration = new SfdxConfiguration('my-project/');
+            const project = new Project(sfdxConfiguration);
+            expect(project.getModuleSourceDirectory()).toBe(
+                'my-project/force-app'
+            );
+        });
+
+        test('when creating from sfdxconfiguration, then use the packageDirectories to resolve the static resources directory', () => {
+            mock({
+                'my-project': {
+                    'sfdx-project.json': JSON.stringify({
+                        packageDirectories: [
+                            {
+                                path: 'force-app',
+                                default: true
+                            }
+                        ]
+                    }),
+                    'localdevserver.config.json': '{}'
+                }
+            });
+            const sfdxConfiguration = new SfdxConfiguration('my-project/');
+            const project = new Project(sfdxConfiguration);
+            expect(project.getStaticResourcesDirectory()).toBe(
+                'my-project/force-app/main/default/staticresources'
+            );
+        });
+
+        test('when creating from sfdxconfiguration, then configure the port from the configuration', () => {
+            mock({
+                'my-project': {
+                    'sfdx-project.json': '{}',
+                    'localdevserver.config.json': '{}'
+                }
+            });
+            const sfdxConfiguration = new SfdxConfiguration('my-project/');
+            sfdxConfiguration.port = 123456;
+            const project = new Project(sfdxConfiguration);
+            expect(project.getConfiguration().port).toBe(
+                sfdxConfiguration.port
+            );
+        });
+
+        test('when creating from sfdxconfiguration, then configure the namespace from the configuration', () => {
+            mock({
+                'my-project': {
+                    'sfdx-project.json': '{}',
+                    'localdevserver.config.json': '{}'
+                }
+            });
+            const sfdxConfiguration = new SfdxConfiguration('my-project/');
+            sfdxConfiguration.namespace = 'my-project-namespace';
+            const project = new Project(sfdxConfiguration);
+            expect(project.getConfiguration().namespace).toBe(
+                sfdxConfiguration.namespace
+            );
         });
     });
 });
