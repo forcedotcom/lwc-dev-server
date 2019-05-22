2bd77eb369e009e3b2dc1f385c4f18521fc60dbb
Luke Inman-Semerau <linman@salesforce.com>
2019-05-22 16:32:22 -0700

    update jest config, don't track code coverage on integration tests

--- integration-tests/jest.config.integration-test.js
+++ integration-tests/jest.config.integration-test.js
@@ -1,24 +1,18 @@
 module.exports = {
-    preset: '@lwc/jest-preset',
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
     },
     testMatch: ['<rootDir>/**/?(*.)(spec|test).(js|ts)'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testPathIgnorePatterns: [
-        '/node_modules/',
-        '/lib/',
-        '/dist/',
-        '/test-projects/'
-    ],
-    collectCoverage: true,
-    coverageReporters: ['json', 'html', 'text'],
-    collectCoverageFrom: ['src/**/*.ts', 'src/**/*.js', 'modules/**/*.js'],
-    coveragePathIgnorePatterns: ['prismjs.js'],
-    coverageDirectory: 'reports/coverage',
+    moduleFileExtensions: ['ts', 'js'],
     reporters: [
         'default',
-        ['jest-junit', { output: './reports/junit/jest-results.xml' }]
+        [
+            'jest-junit',
+            {
+                output:
+                    '<rootDir>/../reports/junit/jest-integration-tests-results.xml'
+            }
+        ]
     ],
     setupFilesAfterEnv: ['<rootDir>/setup.ts']
 };
