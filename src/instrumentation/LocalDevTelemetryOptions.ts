cf12d5739ab8c01d5f9a87e18b46e0f87acef2a1
Kris <kgray@salesforce.com>
2019-08-27 16:40:36 -0700

    Initial telemetry library

---
+++ src/instrumentation/LocalDevTelemetryOptions.ts
@@ -0,0 +1,6 @@
+import { TelemetryOptions } from '@salesforce/telemetry/lib/telemetryReporter';
+
+export default class LocalDevTelemetryOptions implements TelemetryOptions {
+    project = 'local-dev-server';
+    key = '0661613d-a529-4a9c-8309-e4f42d94856d';
+}
