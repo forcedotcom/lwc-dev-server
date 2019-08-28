48d2a7f0751d3bbab5072dd1f78ecb124d7814c4
Kris <kgray@salesforce.com>
2019-08-28 11:34:27 -0700

    With tests

---
+++ src/instrumentation/__tests__/LocalDevTelemetryOptions.test.ts
@@ -0,0 +1,15 @@
+import LocalDevTelemetryOptions from '../LocalDevTelemetryOptions';
+
+describe('LocalDevTelemetryOptions', () => {
+    test('uses proper project name', () => {
+        const options = new LocalDevTelemetryOptions();
+
+        expect(options.project).toBe('local-dev-server');
+    });
+
+    test('uses local dev server telemetry key', () => {
+        const options = new LocalDevTelemetryOptions();
+
+        expect(options.key).toBe('0661613d-a529-4a9c-8309-e4f42d94856d');
+    });
+});
