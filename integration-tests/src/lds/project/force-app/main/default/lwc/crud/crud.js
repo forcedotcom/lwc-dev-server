a3bb7f09e13c945e3c296aee58a45bbe456a24ea
Nathan McWilliams <nmcwilliams@salesforce.com>
2019-09-05 16:13:20 -0400

    updates for lds test

--- integration-tests/src/lds/project/force-app/main/default/lwc/crud/crud.js
+++ integration-tests/src/lds/project/force-app/main/default/lwc/crud/crud.js
@@ -1,49 +1,90 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import ID_FIELD from '@salesforce/schema/Account.Id';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
 import {
-    // getRecord, TODO - LDS does not support invoking getRecord directly, what do customers do?
+    getRecord,
     createRecord,
     updateRecord,
-    deleteRecord
+    deleteRecord,
+    getFieldValue
 } from 'lightning/uiRecordApi';
 
-export default class Hello extends LightningElement {
-    @track readId = 'not-set';
-    @track readData = 'not-set';
+export default class Crud extends LightningElement {
+    @track recordId;
+    @track recordName;
+    @track lastEdit;
+    @track lastDelete;
+    @track error;
 
-    create() {
-        const nameValue = this.template.querySelector('.name-input').value;
-        if (nameValue) {
-            createRecord({
-                apiName: 'Account',
-                fields: { Name: nameValue }
-            }).then(Account => {
-                this.readId = Account.id;
-                this.readData = Account.fields.Name.value;
-            });
-        }
+    @wire(getRecord, { recordId: '$recordId', fields: [NAME_FIELD] })
+    wiredRecord;
+
+    get wiredName() {
+        return getFieldValue(this.wiredRecord.data, NAME_FIELD) || 'undefined';
     }
 
-    // read() {
-    //     getRecord()
-    // }
+    create() {
+        const nameValue = this.template.querySelector('.create-input').value;
+        if (!nameValue) {
+            throw new Error(
+                'Cannot call create before setting value in the input field'
+            );
+        }
+
+        const params = {
+            apiName: ACCOUNT_OBJECT.objectApiName,
+            fields: {
+                [NAME_FIELD.fieldApiName]: nameValue
+            }
+        };
+
+        createRecord(params)
+            .then(record => {
+                this.recordId = record.id;
+                this.recordName = getFieldValue(record, NAME_FIELD);
+            })
+            .catch(error => {
+                this.error = JSON.stringify(error, null, 2);
+            });
+    }
 
     edit() {
-        const valueInput = this.template.querySelector('.value-input').value;
-        updateRecord({
+        const nameValue = this.template.querySelector('.edit-input').value;
+        if (!nameValue) {
+            throw new Error(
+                'Cannot call edit without a valid value in the input field'
+            );
+        }
+        if (!this.recordId || this.recordId === 'not-set') {
+            throw new Error('cannot call edit when the recordId is not set');
+        }
+
+        const params = {
             fields: {
-                Id: this.readId,
-                Name: valueInput
+                [ID_FIELD.fieldApiName]: this.recordId,
+                [NAME_FIELD.fieldApiName]: nameValue
             }
-        }).then(record => {
-            this.readData = record.fields.Name.value;
-        });
+        };
+
+        updateRecord(params)
+            .then(record => {
+                this.recordName = getFieldValue(record, NAME_FIELD);
+                this.lastEdit = Date.now();
+            })
+            .catch(error => {
+                this.error = JSON.stringify(error, null, 2);
+            });
     }
 
     // short for delete, which is a keyword and not a good one for a function name
     delet() {
-        deleteRecord(this.readId).then(() => {
-            this.readId = 'deleted';
-            this.readData = 'deleted';
-        }); // TODO error handling?
+        deleteRecord(this.recordId)
+            .then(() => {
+                this.lastDelete = Date.now();
+            })
+            .catch(error => {
+                this.error = JSON.stringify(error, null, 2);
+            });
     }
 }
