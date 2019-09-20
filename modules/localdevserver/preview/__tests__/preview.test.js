699ee7a5ae8a2724c5e4521bc111e2c66fb5436e
Nathan McWilliams <nmcwilliams@salesforce.com>
2019-09-19 22:39:35 -0400

    preview header buttons + tests

---
+++ modules/localdevserver/preview/__tests__/preview.test.js
@@ -0,0 +1,67 @@
+import { createElement } from 'lwc';
+import Preview from 'localdevserver/preview';
+import { createElement as talonCreateElement } from 'talon/componentService';
+
+// this is indirectly imported by talon framework stuff, and needs to be mocked!
+jest.mock('@talon/connect-gen/dist/forceChatterApi/util/util', () => ({}), {
+    virtual: true
+});
+
+jest.mock('talon/componentService', () => ({
+    createElement: jest.fn()
+}));
+
+async function waitForRender() {
+    return new Promise(resolve => {
+        process.nextTick(resolve);
+    });
+}
+
+function createComponentUnderTest(props) {
+    const el = createElement('localdevserver-preview', { is: Preview });
+    Object.assign(el, props);
+    document.body.appendChild(el);
+    return el;
+}
+
+describe('preview', () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('renders', () => {
+        const componentElement = createComponentUnderTest();
+        expect(componentElement).toMatchSnapshot();
+    });
+
+    it('renders a component', async () => {
+        talonCreateElement.mockImplementation(() => {
+            const el = document.createElement('div');
+            el.appendChild(document.createTextNode('test element'));
+            return Promise.resolve(el);
+        });
+
+        const componentElement = createComponentUnderTest({
+            cmp: 'c/foo'
+        });
+
+        await waitForRender();
+
+        expect(componentElement).toMatchSnapshot();
+    });
+
+    it('displays errors', async () => {
+        talonCreateElement.mockImplementation(() => {
+            const el = document.createElement('div');
+            el.appendChild(document.createTextNode('test element'));
+            return Promise.reject('test error');
+        });
+
+        const componentElement = createComponentUnderTest({
+            cmp: 'c/foo'
+        });
+
+        await waitForRender();
+        expect(componentElement).toMatchSnapshot();
+    });
+});
