4183652c39182e46c10c8bdb131cf64c49b98370
Kris Gray <kgray@salesforce.com>
2019-10-12 16:22:59 -0700

    Fix tests for TelemetryOptions

--- src/instrumentation/__tests__/LocalDevTelemetryOptions.test.ts
+++ src/instrumentation/__tests__/LocalDevTelemetryOptions.test.ts
@@ -2,14 +2,26 @@ import LocalDevTelemetryOptions from '../LocalDevTelemetryOptions';
 
 describe('LocalDevTelemetryOptions', () => {
     test('uses proper project name', () => {
-        const options = new LocalDevTelemetryOptions();
+        const options = new LocalDevTelemetryOptions('userId', 'sessionId');
 
         expect(options.project).toBe('lwc-dev-server');
     });
 
     test('uses local dev server telemetry key', () => {
-        const options = new LocalDevTelemetryOptions();
+        const options = new LocalDevTelemetryOptions('userId', 'sessionId');
 
         expect(options.key).toBe('f132481a-8211-46a8-9df1-c15d05ac3507');
+    });
+
+    test('specifies userId on contextTags', () => {
+        const options = new LocalDevTelemetryOptions('userId', 'sessionId');
+
+        expect(options.contextTags['ai.user.id']).toBe('userId');
+    });
+
+    test('specifies sessionId on contextTags', () => {
+        const options = new LocalDevTelemetryOptions('userId', 'sessionId');
+
+        expect(options.contextTags['ai.session.id']).toBe('sessionId');
     });
 });
