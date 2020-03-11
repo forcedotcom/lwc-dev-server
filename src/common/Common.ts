aaa88ae7f5e4e355804f5d3009ca2e535ec91e93
Raj Rao <rao.r@salesforce.com>
2020-03-10 19:49:34 -0700

    Added some tests

--- src/common/Common.ts
+++ src/common/Common.ts
@@ -1,7 +1,10 @@
 export class MapUtils {
     static filter<K, V>(map: Map<K, V>, predicate: (k: K, v: V) => boolean) {
-        const entries = Array.from(map.entries());
         const aMap = new Map<K, V>();
+        if (map == undefined || map == null) {
+            return aMap;
+        }
+        const entries = Array.from(map.entries());
         for (let [key, value] of entries) {
             if (predicate(key, value) == true) {
                 aMap.set(key, value);
@@ -13,8 +16,11 @@ export class MapUtils {
 
 export class SetUtils {
     static filter<V>(set: Set<V>, predicate: (v: V) => boolean) {
-        const entries = Array.from(set.entries());
         const aSet = new Set<V>();
+        if (set == undefined || set == null) {
+            return aSet;
+        }
+        const entries = Array.from(set.entries());
         for (const [value] of entries) {
             if (predicate(value) == true) aSet.add(value);
         }
