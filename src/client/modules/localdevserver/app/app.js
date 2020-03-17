38946e503223d2b3fb626794372c81928eabd850
Nathan McWilliams <nmcwilliams@salesforce.com>
2020-03-17 18:01:07 -0400

    initial setup

--- src/client/modules/localdevserver/app/app.js
+++ src/client/modules/localdevserver/app/app.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, register } from 'lwc';
+import { registerWireService } from 'wire-service';
+import { createRouter } from 'webruntime_navigation/navigation';
+
+import routes from './routes';
+
+registerWireService(register);
+
+const router = createRouter({ routes });
+
+router.addPostNavigate(data => {
+    console.debug('post navigate event', data);
+});
+
+router.addErrorNavigate(e => {
+    console.error(
+        `There was a problem during navigation: ${e.code} :: ${e.message}`
+    );
+});
+
+router.connect();
 
 export default class LocalDevServerApp extends LightningElement {}
---
+++ src/client/modules/localdevserver/app/routes.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        id: 'home',
+        path: '/',
+        component: 'localdevserver/home'
+    },
+    {
+        id: 'preview',
+        path: '/preview/:descriptor',
+        component: 'localdevserver/preview'
+    }
+];
