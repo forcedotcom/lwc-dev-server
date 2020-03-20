1fdb0a262c9dcd8afcb1143b06f693e5365ce05b
Nathan McWilliams <nmcwilliams@salesforce.com>
2020-03-20 18:57:01 -0400

    addressable service for custom components

---
+++ src/server/services/CustomComponentService.ts
@@ -0,0 +1,116 @@
+import path from 'path';
+import debugLogger from 'debug';
+import { resolveModules } from '@lwc/module-resolver';
+import { compile } from '@webruntime/compiler';
+
+// TODO: change to import and remove workaround for webruntime typescript support
+const { AddressableService, RequestOutputTypes } = require('@webruntime/api');
+export { AddressableService };
+
+const SFDX_LWC_DIRECTORY = 'lwc';
+
+const URI_PREFIX = `/custom-component`;
+const URI = `${URI_PREFIX}/:uid/:mode/:locale/:namespace/:name`;
+
+const debug = debugLogger('localdevserver:customcomponents');
+
+/**
+ * Returns a Service which will resolve custom components from an SFDX project.
+ *
+ * The custom components live in a directory (`lwc`) that differs from the
+ * namespace referenced in code (usually `c`). This maps references to that
+ * namespace to the actual directory the compiler expects.
+ *
+ * @param customModulesNamespace The custom components namespace, e.g., `c`.
+ * @param modulesDirectory Absolute path to the parent of the 'lwc' directory.
+ */
+export function getCustomComponentService(
+    customModulesNamespace: string,
+    modulesDirectory: string
+) {
+    return class CustomComponentService extends AddressableService {
+        constructor(config: any) {
+            super(URI);
+        }
+
+        get mappings() {
+            const modules = this.resolveCustomModules();
+            debug(`found modules: ${JSON.stringify(modules, null, 2)}`);
+
+            const mappings: { [key: string]: any } = {};
+            for (const mapping of modules) {
+                const { specifier } = mapping;
+                if (specifier) {
+                    const name = extractNameFromSpecifier(specifier);
+                    if (!name) {
+                        throw new Error(
+                            `Invalid specifier for custom component: ${specifier}`
+                        );
+                    }
+                    const key = `${customModulesNamespace}/${name}`;
+                    mappings[key] = `${URI_PREFIX}/:uid/:mode/:locale/${key}`;
+                }
+            }
+            debug(`mappings: ${JSON.stringify(mappings, null, 2)}`);
+            return mappings;
+        }
+
+        toSpecifier(url: string): string {
+            const { namespace, name } = this.parseUrl(url);
+            return `${namespace}/${name}`;
+        }
+
+        async request(
+            specifier: string,
+            pivots: any,
+            { compilerConfig }: any
+        ): Promise<any> {
+            debug(`Received request for ${specifier}`);
+
+            const name = extractNameFromSpecifier(specifier);
+            if (!name) {
+                throw new Error(
+                    `Invalid specifier for custom component: ${specifier}`
+                );
+            }
+
+            compilerConfig.baseDir = modulesDirectory;
+
+            const { result, metadata, success, diagnostics } = await compile({
+                ...compilerConfig,
+                namespace: SFDX_LWC_DIRECTORY,
+                name
+            });
+
+            return {
+                type: RequestOutputTypes.COMPONENT,
+                specifier,
+                resource: result,
+                metadata,
+                success,
+                diagnostics
+            };
+        }
+
+        resolveCustomModules() {
+            const lwcModulesDirectory = path.join(
+                modulesDirectory,
+                SFDX_LWC_DIRECTORY
+            );
+
+            const customModules = resolveModules({
+                modules: [modulesDirectory]
+            }).filter(mapping => mapping.entry.startsWith(lwcModulesDirectory));
+
+            return customModules;
+        }
+    };
+}
+
+function extractNameFromSpecifier(specifier: string) {
+    const split = specifier.split('/');
+    if (!split || split.length !== 2) {
+        return null;
+    }
+    return split[1];
+}
