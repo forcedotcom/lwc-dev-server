784f66123c23a8d2bb2f0ae5937aa690035c2bb8
Brian Buchanan <bbuchanan@salesforce.com>
2020-04-17 14:43:32 -0400

    mock webruntime client modules

--- src/client/modules/localdevserver/app/__tests__/app.test.js
+++ src/client/modules/localdevserver/app/__tests__/app.test.js
@@ -1,6 +1,22 @@
 import { createElement } from 'lwc';
 import App from 'localdevserver/app';
 
+jest.mock('webruntime/aura', () => ({}), { virtual: true });
+jest.mock('webruntime/auraStorage', () => ({}), { virtual: true });
+jest.mock('webruntime/auraInstrumentation', () => ({}), { virtual: true });
+jest.mock('webruntime/transport', () => ({}), { virtual: true });
+jest.mock('webruntime/logger', () => ({}), { virtual: true });
+
+jest.mock(
+    'webruntime_loader/loader',
+    () => {
+        return {
+            defineModules: jest.fn()
+        };
+    },
+    { virtual: true }
+);
+
 function createComponentUnderTest(props) {
     const el = createElement('localdevserver-app', {
         is: App
