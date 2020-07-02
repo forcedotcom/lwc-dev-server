9fa8f78a1c719041c256663eb1945a2a3c03250c
Xiaoyi Chen <xchen@salesforce.com>
2020-07-02 15:28:32 -0700

    remove non-sfdx logic in component index

--- src/common/ComponentIndex.ts
+++ src/common/ComponentIndex.ts
@@ -41,10 +41,6 @@ export default class ComponentIndex {
                     `no 'lwc' directory found in path ${modulesSourceDirectory}`
                 );
             }
-        } else {
-            moduleDirectories.push(
-                ...this.findSubdirectories(modulesSourceDirectory)
-            );
         }
         return this.findModulesIn(moduleDirectories);
     }
