6c88ad425b3bd3d44479b5b493aba3e7e321e184
Shelby Holden <shelby.holden@salesforce.com>
2020-07-16 16:07:42 -0600

    Reverted changes meant only for testing

--- script/publish-workflow.sh
+++ script/publish-workflow.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 CircleCIToken=$1
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
-  "branch": "sh/configImprovements",
+  "branch": "main",
   "parameters": {
     "publish": true
   }
