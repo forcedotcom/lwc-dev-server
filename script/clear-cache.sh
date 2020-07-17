09fabe57f782ac87c8b478d1988256cc9771cc68
Shelby Holden <shelby.holden@salesforce.com>
2020-07-17 14:39:21 -0600

    Reverted image change for build

--- script/clear-cache.sh
+++ script/clear-cache.sh
@@ -3,6 +3,6 @@ CircleCIToken=$1
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "main",
   "parameters": {
-    "cache-version": "v2"
+    "cache-version": "v3"
   }
 }' https://circleci.com/api/v2/project/gh/forcedotcom/lwc-dev-server/pipeline
