7d8d3961a2516a24d1fe993e8048565f647dafee
Shelby Holden <shelby.holden@salesforce.com>
2020-08-05 14:18:10 -0600

    Added comma separation

--- script/publish-workflow.sh
+++ script/publish-workflow.sh
@@ -8,7 +8,7 @@ PublishType=$2
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d `{
   "branch": "main",
   "parameters": {
-    "publish": true
+    "publish": true,
     "publish-type": ${PublishType}
   }
 }` https://circleci.com/api/v2/project/gh/forcedotcom/lwc-dev-server/pipeline
