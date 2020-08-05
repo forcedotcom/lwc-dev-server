3addc7094e7fd2eaf8482db371f6935e1eb006fa
Shelby Holden <shelby.holden@salesforce.com>
2020-08-05 13:48:36 -0600

    Add support for publishing a patch version

--- script/publish-workflow.sh
+++ script/publish-workflow.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+# For publishing a patch version, add the following parameter:
+# "publish-type": "patch"
+
 CircleCIToken=$1
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "main",
