6456f4f6f789499d3c80a5bf8a4d2c2340b6f22b
Shelby Holden <shelby.holden@salesforce.com>
2020-11-05 13:40:01 -0700

    test: finish tests for emp-api support

--- integration-tests/src/emp-api/emp-api.test.ts
+++ integration-tests/src/emp-api/emp-api.test.ts
@@ -1,17 +1,22 @@
-import fs from 'fs-extra';
-import path from 'path';
 import PreviewPage from '../pageObjects/PreviewPage';
 
 describe('lightning:empApi Component', () => {
-    const lwcFolder = path.join(
-        __dirname,
-        'project/force-app/main/default/lwc'
-    );
-
     it('loads', async () => {
         let page = new PreviewPage('c', 'empApiLWC');
         await page.open();
 
-        const gothere = '';
+        // Verify the label input for 'Channel Name'
+        const lightningInputLabel = await page.testComponent
+            .then(el => el.shadow$('lightning-card'))
+            .then(el => el.$('lightning-input')); 
+        expect(await lightningInputLabel.getText()).toBe('Channel Name');
+
+        // Verify the subscribe and unsubscribe lightning buttons are present
+        const lightningButtons = await page.testComponent
+            .then(el => el.shadow$('lightning-card'))
+            .then(el => el.$$('lightning-button'));
+        expect(lightningButtons.length).toEqual(2);
+        expect(await lightningButtons[0].shadow$('button[title="Subscribe"')).toBeTruthy();
+        expect(await lightningButtons[1].shadow$('button[title="Unsubscribe"')).toBeTruthy();
     });
 });
