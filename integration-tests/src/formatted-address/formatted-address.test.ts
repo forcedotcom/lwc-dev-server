21fbccf139b112f062d6a9790e5963439c57cf2b
Shelby Holden <shelby.holden@salesforce.com>
2020-11-05 14:47:18 -0700

    test: add tests for formatted-address

--- integration-tests/src/formatted-address/formatted-address.test.ts
+++ integration-tests/src/formatted-address/formatted-address.test.ts
@@ -3,45 +3,27 @@ import path from 'path';
 import PreviewPage from '../pageObjects/PreviewPage';
 
 describe('lightning:formatted-address Component', () => {
-    const lwcFolder = path.join(
-        __dirname,
-        'project/force-app/main/default/lwc'
-    );
-
     it('loads', async () => {
         let page = new PreviewPage('c', 'formattedAddress');
         await page.open();
 
-        const parent = await page.testComponent.then(el =>
-            el.shadow$('lightning-formatted-address')
-        );
+        const formattedAddresses = await page.testComponent.then(el => el.shadow$$('lightning-formatted-address'));
+        expect(formattedAddresses.length).toBe(2);
 
-        expect(parent.getSize()).toBe(2);
+        // Verify address1 contains a google link
+        const googleLinkElement = await formattedAddresses[0].shadow$('a');
+        expect(await googleLinkElement.getText()).toBe('121 Spear St.\nSan Francisco, CA 94105\nUS');
+        expect(await googleLinkElement.getAttribute('href')).toBe('https://www.google.com/maps?q=121%20Spear%20St.%0ASan%20Francisco,%20CA%2094105%0AUS');
 
-        // Grab the google link reference
-        const googleLink = await page.testComponent
-            .then(el => el.shadow$('lightning-formatted-address'))
-            .then(el => el.$('a'));
-        // $$ - no elements found (but doesn't error)
-        // $ - finds an element, but errors with '"no such element: Unable to locate element: {\"method\":\"css selector\",\"selector\":\"a\"}\n  (Session info: chrome=86.0.4240.183)\n  (Driver info: chromedriver=2.43.600229 (3fae4d0cda5334b4f533bede5a4787f7b832d052),platform=Mac OS X 10.14.6 x86_64)"'
+        // Verify address2 is plain text
+        const plainTextElement = formattedAddresses[1];
+        const children = await plainTextElement.shadow$$('*');
+        expect(children.length).toBe(3);
+        expect(await plainTextElement.$$('*').length).toBe(undefined);
 
-        // There should only be one link available within lightning-formatted-address
-        // expect(googleLink.length).toBe(1);   Size is 0 when using $$.
-        expect(googleLink.getSize()).toBe(1);
-
-        // const link = googleLink.getAttribute('href');
-
-        // const googleLink = await page.testComponent.then(el =>
-        //     el.shadow$('lightning-formatted-address').getAttribute('href')
-        // );
-
-        // const googleLink = await page.testComponent.then(el =>
-        //     el.shadow$('lightning-formatted-address').getHTML()
-        // );
-
-        // TODO - verify google link text is:
-        //https://www.google.com/maps?q=121%20Spear%20St.%0ASan%20Francisco,%20CA%2094105%0AUS
-
-        const gothere = '';
+        // Verify address2 only contains children that are plain text
+        expect(await children[0].getHTML()).toBe('<div class="slds-truncate">121 Spear St.</div>');
+        expect(await children[1].getHTML()).toBe('<div class="slds-truncate">San Francisco, CA 94105</div>');
+        expect(await children[2].getHTML()).toBe('<div class="slds-truncate">US</div>');
     });
 });
