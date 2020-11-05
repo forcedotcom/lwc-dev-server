ecb14c8b28eff1e1057312be539324555f061eb1
Shelby Holden <shelby.holden@salesforce.com>
2020-11-05 15:22:16 -0700

    test: added prettier formatting

--- integration-tests/src/formatted-address/formatted-address.test.ts
+++ integration-tests/src/formatted-address/formatted-address.test.ts
@@ -5,13 +5,19 @@ describe('lightning:formatted-address Component', () => {
         let page = new PreviewPage('c', 'formattedAddress');
         await page.open();
 
-        const formattedAddresses = await page.testComponent.then(el => el.shadow$$('lightning-formatted-address'));
+        const formattedAddresses = await page.testComponent.then(el =>
+            el.shadow$$('lightning-formatted-address')
+        );
         expect(formattedAddresses.length).toBe(2);
 
         // Verify address1 contains a google link
         const googleLinkElement = await formattedAddresses[0].shadow$('a');
-        expect(await googleLinkElement.getText()).toBe('121 Spear St.\nSan Francisco, CA 94105\nUS');
-        expect(await googleLinkElement.getAttribute('href')).toBe('https://www.google.com/maps?q=121%20Spear%20St.%0ASan%20Francisco,%20CA%2094105%0AUS');
+        expect(await googleLinkElement.getText()).toBe(
+            '121 Spear St.\nSan Francisco, CA 94105\nUS'
+        );
+        expect(await googleLinkElement.getAttribute('href')).toBe(
+            'https://www.google.com/maps?q=121%20Spear%20St.%0ASan%20Francisco,%20CA%2094105%0AUS'
+        );
 
         // Verify address2 is plain text
         const plainTextElement = formattedAddresses[1];
@@ -20,8 +26,14 @@ describe('lightning:formatted-address Component', () => {
         expect(await plainTextElement.$$('*').length).toBe(undefined);
 
         // Verify address2 only contains children that are plain text
-        expect(await children[0].getHTML()).toBe('<div class="slds-truncate">121 Spear St.</div>');
-        expect(await children[1].getHTML()).toBe('<div class="slds-truncate">San Francisco, CA 94105</div>');
-        expect(await children[2].getHTML()).toBe('<div class="slds-truncate">US</div>');
+        expect(await children[0].getHTML()).toBe(
+            '<div class="slds-truncate">121 Spear St.</div>'
+        );
+        expect(await children[1].getHTML()).toBe(
+            '<div class="slds-truncate">San Francisco, CA 94105</div>'
+        );
+        expect(await children[2].getHTML()).toBe(
+            '<div class="slds-truncate">US</div>'
+        );
     });
 });
