cfbe2fa705d963e64fe3916513ed476585a0e474
Shelby Holden <shelby.holden@salesforce.com>
2020-12-09 13:24:27 -0700

    test: tests in progress

--- src/server/extensions/__tests__/projectMetadata.test.ts
+++ src/server/extensions/__tests__/projectMetadata.test.ts
@@ -2,8 +2,14 @@ import express, { Application } from 'express';
 import { projectMetadata } from '../projectMetadata';
 import Project from '../../../common/Project';
 import { ExtensionOptions } from '@webruntime/api';
+import path from 'path';
+// import * as fileUtils from '../../../common/fileUtils';
+import fs from 'fs';
+import mockFs from 'mock-fs';
 
 jest.mock('../../../common/Project');
+
+// jest.mock('../../../common/fileUtils');
 
 jest.mock('express', () => {
     return jest.fn(() => {
@@ -16,6 +22,7 @@ jest.mock('express', () => {
 
 describe('projectMetadata', () => {
     let project: Project;
+    // let fileUtilsDeleteMock: any;
     const nonce = 'sessionNonce';
 
     beforeEach(() => {
@@ -45,6 +52,28 @@ describe('projectMetadata', () => {
             const route = app.get.mock.calls[0][0];
 
             expect(route).toEqual(`/localdev/${nonce}/localdev.js`);
+        });
+
+        it('should clear dev folder for localdev.js route', () => {
+            const devFolder = path.join(
+                project.projectDirectory,
+                '.localdevserver',
+                'webruntime',
+                'dev',
+                'en-US'
+            );
+            const extension = projectMetadata(nonce, project);
+            mockFs({
+                [`${devFolder}`]: {}
+            });
+            expect(fs.existsSync(devFolder)).toBeTruthy();
+
+            extension.extendApp({ app, options });
+
+            // @ts-ignore
+            app.use.mock.calls[0][0];
+
+            expect(fs.existsSync(devFolder)).toBeFalsy();
         });
 
         it('should add the /localdev/{{sessionNonce}}errorDetails route', async () => {
