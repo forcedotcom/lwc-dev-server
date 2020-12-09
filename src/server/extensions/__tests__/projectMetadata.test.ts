f3869b6aebd0e624922d6a7ed843b2d6137ad7f3
Shelby Holden <shelby.holden@salesforce.com>
2020-12-09 16:49:34 -0700

    test: add test for verifying dev folder removal

--- src/server/extensions/__tests__/projectMetadata.test.ts
+++ src/server/extensions/__tests__/projectMetadata.test.ts
@@ -3,13 +3,11 @@ import { projectMetadata } from '../projectMetadata';
 import Project from '../../../common/Project';
 import { ExtensionOptions } from '@webruntime/api';
 import path from 'path';
-// import * as fileUtils from '../../../common/fileUtils';
+import * as fileUtils from '../../../common/fileUtils';
 import fs from 'fs';
 import mockFs from 'mock-fs';
 
 jest.mock('../../../common/Project');
-
-// jest.mock('../../../common/fileUtils');
 
 jest.mock('express', () => {
     return jest.fn(() => {
@@ -22,7 +20,6 @@ jest.mock('express', () => {
 
 describe('projectMetadata', () => {
     let project: Project;
-    // let fileUtilsDeleteMock: any;
     const nonce = 'sessionNonce';
 
     beforeEach(() => {
@@ -55,25 +52,46 @@ describe('projectMetadata', () => {
         });
 
         it('should clear dev folder for localdev.js route', () => {
+            const extension = projectMetadata(nonce, project);
+            extension.extendApp({ app, options });
+
+            jest.spyOn(fileUtils, 'removeFile').mockImplementation();
+
             const devFolder = path.join(
                 project.projectDirectory,
                 '.localdevserver',
                 'webruntime',
+                'custom-component',
                 'dev',
                 'en-US'
             );
-            const extension = projectMetadata(nonce, project);
-            mockFs({
-                [`${devFolder}`]: {}
-            });
-            expect(fs.existsSync(devFolder)).toBeTruthy();
+            const devFile = path.join(devFolder, 'c', 'sample.js');
 
-            extension.extendApp({ app, options });
+            mockFs({
+                [`${project.projectDirectory}/package.json`]: JSON.stringify({
+                    name: 'test-project'
+                }),
+                [`${devFile}`]: 'errors you do not want'
+            });
+
+            const req: any = {
+                url: `/localdev/${nonce}/localdev.js`,
+                params: {
+                    '0': 'localdev.js'
+                }
+            };
+            const res: any = {
+                type: jest.fn(),
+                send: jest.fn()
+            };
+            const next = jest.fn();
 
             // @ts-ignore
-            app.use.mock.calls[0][0];
+            const routeHandler = (app.get as jest.Mock).mock.calls[0][1];
+            routeHandler(req, res, next);
 
-            expect(fs.existsSync(devFolder)).toBeFalsy();
+            expect(fileUtils.removeFile).toHaveBeenCalledTimes(1);
+            expect(fileUtils.removeFile).toHaveBeenCalledWith(devFolder);
         });
 
         it('should add the /localdev/{{sessionNonce}}errorDetails route', async () => {
