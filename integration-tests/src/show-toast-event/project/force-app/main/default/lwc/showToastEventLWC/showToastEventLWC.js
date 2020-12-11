6da98e18167e807e22a80962e2eb6ce506ccaa5f
Shelby Holden <45604118+sfsholden@users.noreply.github.com>
2020-12-10 17:16:50 -0700

    test: add integration test for platformShowToastEvent (#297)

---
+++ integration-tests/src/show-toast-event/project/force-app/main/default/lwc/showToastEventLWC/showToastEventLWC.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+export default class ShowToastEventLWC extends LightningElement {
+    showToast() {
+        const event = new ShowToastEvent({
+            title: 'Get Help',
+            message:
+                'Salesforce documentation is available in the app. Click ? in the upper-right corner.'
+        });
+        this.dispatchEvent(event);
+    }
+}
