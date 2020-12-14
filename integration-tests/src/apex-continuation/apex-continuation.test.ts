18be96b83cb0e16f1f75f4142b57850489ff4580
Shelby Holden <shelby.holden@salesforce.com>
2020-12-14 10:24:46 -0700

    test: fix hardcoded path

--- integration-tests/src/apex-continuation/apex-continuation.test.ts
+++ integration-tests/src/apex-continuation/apex-continuation.test.ts
@@ -1,11 +1,13 @@
 import PreviewPage from '../pageObjects/PreviewPage';
+import path from 'path';
 
 describe('@salesforce/apexContinuation Component', () => {
-    
-    const errorText =
-        "Error during compilation: Could not load @salesforce/apexContinuation/ApexContinuationClass.startRequest (imported by /Users/shelby.holden/workspace/local-dev/lwc-dev-server/integration-tests/src/apex-continuation/project/force-app/main/default/lwc/apexContinuation/apexContinuation.js): You can't preview a component using Apex Continuation. Test the component directly in the org";
-
     it('loads', async () => {
+        const errorText =
+            'Error during compilation: Could not load @salesforce/apexContinuation/ApexContinuationClass.startRequest (imported by ' +
+            __dirname +
+            "/project/force-app/main/default/lwc/apexContinuation/apexContinuation.js): You can't preview a component using Apex Continuation. Test the component directly in the org";
+
         let page = new PreviewPage('c', 'apexContinuation');
 
         await page.open();
