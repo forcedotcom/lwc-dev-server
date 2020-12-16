76cf41f3f932e217bba14b06afb733fc1d91a8e3
Lisandro Fernandez <lisandro.fernandez@salesforce.com>
2020-12-16 14:36:01 -0300

    fix: improving the test cases

--- integration-tests/src/lightning-message-service/lightning-message-service.test.ts
+++ integration-tests/src/lightning-message-service/lightning-message-service.test.ts
@@ -2,9 +2,14 @@ import PreviewPage from '../pageObjects/PreviewPage';
 
 describe('lightning:lightningMessageService Component', () => {
     it('loads', async () => {
+        const errorText =
+            "The component named 'c/lightningMessageService' was not found. Only components within the project namespace can be previewed.";
         let page = new PreviewPage('c', 'lightningMessageService');
-        const document = await page.open();
-        const errorElement = await document.$$('localdevserver-error');
-        expect(errorElement.length).toBe(1);
+        await page.open();
+        const errorHeader = await page.container
+            .then(el => el.$('localdevserver-error'))
+            .then(el => el.shadow$('div.slds-modal__container'))
+            .then(el => el.$('h1.error-message.slds-text-heading_large'));
+        expect(await errorHeader.getText()).toBe(errorText);
     });
 });
--- integration-tests/src/misc-permission-based-UI/misc-permission-based-UI.test.ts
+++ integration-tests/src/misc-permission-based-UI/misc-permission-based-UI.test.ts
@@ -3,8 +3,13 @@ import PreviewPage from '../pageObjects/PreviewPage';
 describe('lightning:miscPermissionBasedUI Component', () => {
     it('loads', async () => {
         let page = new PreviewPage('c', 'miscPermissionBasedUI');
-        const document = await page.open();
-        const errorElement = await document.$$('localdevserver-error');
-        expect(errorElement.length).toBe(1);
+        const errorText =
+            "The component named 'c/miscPermissionBasedUI' was not found. Only components within the project namespace can be previewed.";
+        await page.open();
+        const errorHeader = await page.container
+            .then(el => el.$('localdevserver-error'))
+            .then(el => el.shadow$('div.slds-modal__container'))
+            .then(el => el.$('h1.error-message.slds-text-heading_large'));
+        expect(await errorHeader.getText()).toBe(errorText);
     });
 });
