303525329f874c36eca1e8f63e7a1da0d4bd9f37
Lisandro Fernandez <lisandro.fernandez@salesforce.com>
2020-12-17 15:25:30 -0300

    fix: validating the correct error message

--- integration-tests/src/lightning-message-service/project/force-app/main/default/lwc/lightningMessageService/lightningMessageService.js
+++ integration-tests/src/lightning-message-service/project/force-app/main/default/lwc/lightningMessageService/lightningMessageService.js
@@ -1,25 +1,15 @@
 import { LightningElement, wire } from 'lwc';
-import { APPLICATION_SCOPE, 
-    createMessageContext, 
-    MessageContext, 
-    publish, 
-    releaseMessageContext, 
-    subscribe, 
-    unsubscribe } from 'lightning/messageService';
+import { MessageContext, publish } from 'lightning/messageService';
 import SAMPLEMC from '@salesforce/messageChannel/SampleMessageChannel__c';
 
 export default class LightningMessageService extends LightningElement {
     @wire(MessageContext)
     messageContext;
-    
-          
+
     handleClick() {
-        console.log(APPLICATION_SCOPE)
-        
-        debugger;
         const message = {
             recordId: '001xx000003NGSFAA4',
-            recordData: {accountName: 'Burlington Textiles Corp of America'}
+            recordData: { accountName: 'Burlington Textiles Corp of America' }
         };
         publish(this.messageContext, SAMPLEMC, message);
     }
