a177d46ace32e386bd164d3b568af85a4203fe9b
Luis Campos <lcamposguajardo@gmail.com>
2020-12-21 12:42:29 -0800

    fix: preview-bundle move testLib to staticresource

--- integration-tests/src/preview-bundle/project/src/modules/lwc/testComponent/testComponent.js
+++ integration-tests/src/preview-bundle/project/src/modules/lwc/testComponent/testComponent.js
@@ -1,13 +1,26 @@
 import { LightningElement } from 'lwc';
-import { foo } from 'test/testLib';
+import { loadScript } from 'lightning/platformResourceLoader';
+import TEST_LIB from '@salesforce/resourceUrl/testLib';
 import { bar } from './util';
 
 export default class TestComponent extends LightningElement {
     greeting = 'World';
 
-    constructor() {
-        super();
-        foo();
-        bar();
+    testLibInitialized = false;
+
+    renderedCallback() {
+        if (this.testLibInitialized) {
+            return;
+        }
+        this.testLibInitialized = true;
+
+        loadScript(this, TEST_LIB)
+            .then(() => {
+                foo();
+                bar();
+            })
+            .catch(error => {
+                this.error = error;
+            });
     }
 }
