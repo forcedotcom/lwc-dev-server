4d1f2282a01da8d95aa2227ac26a131686469ae3
Lisandro Fernandez <lisandro.fernandez@salesforce.com>
2021-01-29 16:48:21 -0300

    test: validating the text to be equal

--- integration-tests/src/wire-current-page-reference/wire-current-page-reference.test.ts
+++ integration-tests/src/wire-current-page-reference/wire-current-page-reference.test.ts
@@ -4,12 +4,14 @@ describe('wireCurrentPageReference component', () => {
     it('loads', async () => {
         let page = new PreviewPage('c', 'wireCurrentPageReference');
         await page.open();
+        const expectedText =
+            '{\n  "attributes": {\n    "namespace": "c",\n    "name": "wireCurrentPageReference"\n  },\n  "state": {}\n}';
         const content = await page.container.then(el =>
             el.$(
                 '/html/body/localdevserver-app/localdevserver-layout/slot[2]/main/localdevserver-view/localdevserver-dynamic/localdevserver-layout-section/slot/article/div[2]/div/localdevserver-dynamic/lightning-card/article/div[1]/slot/div/pre'
             )
         );
-        const contentText = JSON.parse(await content.getText());
-        expect(contentText.attributes.name).toBe('wireCurrentPageReference');
+        const contentText = await content.getText();
+        expect(contentText).toBe(expectedText);
     });
 });
