1ba1a80dcee5c390ce9664f4a76e7b9229beb466
Lisandro Fernandez <70181900+LisandroFernandezSF@users.noreply.github.com>
2021-01-29 14:43:51 -0300

    fix: validate if the image exists in the DOM (#322)

--- integration-tests/src/misc-content-asset/misc-content-asset.test.ts
+++ integration-tests/src/misc-content-asset/misc-content-asset.test.ts
@@ -13,4 +13,16 @@ describe('MiscContentAsset Component', () => {
         expect(contentText[1]).toBe('Use content assets.');
         expect(contentText[2]).toBe('View Source');
     });
+
+    it('shows the asset', async () => {
+        let page = new PreviewPage('c', 'miscContentAsset');
+        await page.open();
+        const image = await page.container.then(el =>
+            el.$(
+                '/html/body/localdevserver-app/localdevserver-layout/slot[2]/main/localdevserver-view/localdevserver-dynamic/localdevserver-layout-section/slot/article/div[2]/div/localdevserver-dynamic/lightning-card/article/div[1]/slot/div/img'
+            )
+        );
+        const size = await image.getSize();
+        expect(size.width).toBe(128);
+    });
 });
