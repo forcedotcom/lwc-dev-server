658364f4e188d94420f9ac73ae488115a1f89066
Lisandro Fernandez <70181900+LisandroFernandezSF@users.noreply.github.com>
2021-02-16 13:02:45 -0300

    test: compilation error test (#324)
    * test: adding compilation error test
    
    * test: fix test title and add vscode link verification
    
    * test: fix path
    
    Co-authored-by: Shelby Holden <shelby.holden@salesforce.com>

---
+++ integration-tests/src/compile-error/compile-error.test.ts
@@ -0,0 +1,30 @@
+import PreviewPage from '../pageObjects/PreviewPage';
+
+describe('compile-error', () => {
+    it('loads', async () => {
+        const errorTxt =
+            'Invalid HTML syntax: invalid-first-character-of-tag-name. For more information, please visit https://html.spec.whatwg.org/multipage/parsing.html#parse-error-invalid-first-character-of-tag-name';
+        let page = new PreviewPage('c', 'compileError');
+
+        await page.open();
+
+        const error = await page.container.then(el =>
+            el.$(
+                '/html/body/localdevserver-app/localdevserver-layout/slot[2]/main/localdevserver-view/localdevserver-dynamic/localdevserver-layout-section/slot/article/div[2]/localdevserver-error/section/div/div/h1'
+            )
+        );
+        const vscodeLink = await page.container.then(el =>
+            el.$(
+                '/html/body/localdevserver-app/localdevserver-layout/slot[2]/main/localdevserver-view/localdevserver-dynamic/localdevserver-layout-section/slot/article/div[2]/localdevserver-error/section/div/div/h3/a'
+            )
+        );
+
+        expect(await error.getText()).toBe(errorTxt);
+        expect(await vscodeLink.getText()).toBe('Go to source (vscode)');
+        expect(await vscodeLink.getAttribute('href')).toMatch(
+            new RegExp(
+                '^vscode://file.*/integration-tests/src/compile-error/project/force-app/main/default/lwc/compileError/compileError.html:4:9'
+            )
+        );
+    });
+});
